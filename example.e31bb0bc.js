c52e184e0b60966086ec89f85ea831d3605020b6
Artem Tyurin <artem.tyurin@uber.com>
2018-10-23 16:31:08 +0200

    Updates

--- example.e31bb0bc.js
+++ example.e31bb0bc.js
@@ -53326,7 +53326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61932" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56936" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
